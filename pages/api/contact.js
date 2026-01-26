59a1da5b81ca86801576abcbd5ab6e117bb24bc9
Muhammad Shehryar <Shahryartariq57@gmail.com>
2026-01-26 11:51:17 +0500

    Added Neon DB and Added Contact

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,21 +1,20 @@
-function handler(req, res) {
-  if (req.method === "POST") {
-    const { name, email, message } = req.body;
+import { initDb } from "../../lib/initdb";
+import { createContact } from "../../lib/contact";
 
-    if (!email || !email.includes("@") || !name || name.trim() === "" || !message || message.trim() === "") {
-      res.status(422).json({ message: "Invalid input." });
-      return;
-    }
+export default async function handler(req, res) {
+  await initDb(); // GUARANTEED: runs once per server
 
-    // Store it inside a database
-    const newMessage = {
-      email,
-      name,
-      message,
-    };
-
-    console.log(newMessage);
-
-    res.status(201).json({ message: "Successfully Stored Message!", data: newMessage });
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
   }
+
+  const { email, name, message } = req.body;
+
+  if (!email || !name || !message) {
+    return res.status(422).json({ message: "Invalid input" });
+  }
+
+  const contact = await createContact(email, name, message);
+
+  res.status(201).json({ message: "Saved", contact });
 }
