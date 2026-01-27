35a0f8cd7e3bf2d08b3290467e378a6072e94de2
Muhammad Shehryar <Shahryartariq57@gmail.com>
2026-01-27 20:08:32 +0500

    Starting Auth

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,15 +1,23 @@
+import { initDb } from "../../lib/initdb";
 import { createContact } from "../../lib/contact";
 
 export default async function handler(req, res) {
+  await initDb();
+
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
 
-  const { email, name, message } = req.body;
+  const { email, password } = req.body;
 
-  if (!email || !name || !message) {
+  if (!email || !password || !email.includes("@") || password.trim().length < 7 ) {
     return res.status(422).json({ message: "Invalid input" });
   }
+
+  const hashedPassword = await hashPassword(password);
+
+
+
 
   const contact = await createContact(email, name, message);
 
